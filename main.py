b765c88eabb11822c46bc03ffc18509cf0dc3ab1
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-10-02 19:35:44 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#4.10 – Fatias: Usando um dos programas que você escreveu neste capítulo,
+#acrescente várias linhas no final do programa que façam o seguinte: 
+#exibir os três primeiros itens da lista desse programa.
+#• Exiba a mensagem Três itens do meio da lista são
+#• Exiba a mensagem. Use uma fatia para exibir os três últimos itens da lista.
+magicians =['alice', 'roberto', 'alexandre', 'jeremias', 'levin']
+print(magicians[0:3])
+print(magicians[1:4])
+print(magicians[2:])
